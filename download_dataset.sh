8b5fc1e7d28adf36fd3e6e83d648c32f54928104
adi-kiran <adithyakiran1999@gmail.com>
2020-03-31 18:49:03 +0530

    run download_dataset.sh to obtain entire video dataset

---
+++ download_dataset.sh
@@ -0,0 +1,130 @@
+# Download all the required files
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_01.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_02.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_03.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_04.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_05.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_06.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_07.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_08.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_09.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_10.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_11.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_12.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_13.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_14.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_15.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_16.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_17.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_18.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_19.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_20.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_21.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_22.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_23.zip
+wget https://zenodo.org/record/1188976/files/Video_Speech_Actor_24.zip
+
+# unzip them
+unzip Video_Speech_Actor_01.zip
+unzip Video_Speech_Actor_02.zip
+unzip Video_Speech_Actor_03.zip
+unzip Video_Speech_Actor_04.zip
+unzip Video_Speech_Actor_05.zip
+unzip Video_Speech_Actor_06.zip
+unzip Video_Speech_Actor_07.zip
+unzip Video_Speech_Actor_08.zip
+unzip Video_Speech_Actor_09.zip
+unzip Video_Speech_Actor_10.zip
+unzip Video_Speech_Actor_11.zip
+unzip Video_Speech_Actor_12.zip
+unzip Video_Speech_Actor_13.zip
+unzip Video_Speech_Actor_14.zip
+unzip Video_Speech_Actor_15.zip
+unzip Video_Speech_Actor_16.zip
+unzip Video_Speech_Actor_17.zip
+unzip Video_Speech_Actor_18.zip
+unzip Video_Speech_Actor_19.zip
+unzip Video_Speech_Actor_20.zip
+unzip Video_Speech_Actor_21.zip
+unzip Video_Speech_Actor_22.zip
+unzip Video_Speech_Actor_23.zip
+unzip Video_Speech_Actor_24.zip
+
+# make a folder called dataset in previous level
+mkdir ../dataset
+
+# move them to the dataset folder
+mv Actor_01/* ../dataset/
+mv Actor_02/* ../dataset/
+mv Actor_03/* ../dataset/
+mv Actor_04/* ../dataset/
+mv Actor_05/* ../dataset/
+mv Actor_06/* ../dataset/
+mv Actor_07/* ../dataset/
+mv Actor_08/* ../dataset/
+mv Actor_09/* ../dataset/
+mv Actor_10/* ../dataset/
+mv Actor_11/* ../dataset/
+mv Actor_12/* ../dataset/
+mv Actor_13/* ../dataset/
+mv Actor_14/* ../dataset/
+mv Actor_15/* ../dataset/
+mv Actor_16/* ../dataset/
+mv Actor_17/* ../dataset/
+mv Actor_18/* ../dataset/
+mv Actor_19/* ../dataset/
+mv Actor_20/* ../dataset/
+mv Actor_21/* ../dataset/
+mv Actor_22/* ../dataset/
+mv Actor_23/* ../dataset/
+mv Actor_24/* ../dataset/
+
+# remove the unecessary files and folders
+rm -r Actor_01/
+rm -r Actor_02/
+rm -r Actor_03/
+rm -r Actor_04/
+rm -r Actor_05/
+rm -r Actor_06/
+rm -r Actor_07/
+rm -r Actor_08/
+rm -r Actor_09/
+rm -r Actor_10/
+rm -r Actor_11/
+rm -r Actor_12/
+rm -r Actor_13/
+rm -r Actor_14/
+rm -r Actor_15/
+rm -r Actor_16/
+rm -r Actor_17/
+rm -r Actor_18/
+rm -r Actor_19/
+rm -r Actor_20/
+rm -r Actor_21/
+rm -r Actor_22/
+rm -r Actor_23/
+rm -r Actor_24/
+rm Video_Speech_Actor_01.zip
+rm Video_Speech_Actor_02.zip
+rm Video_Speech_Actor_03.zip
+rm Video_Speech_Actor_04.zip
+rm Video_Speech_Actor_05.zip
+rm Video_Speech_Actor_06.zip
+rm Video_Speech_Actor_07.zip
+rm Video_Speech_Actor_08.zip
+rm Video_Speech_Actor_09.zip
+rm Video_Speech_Actor_10.zip
+rm Video_Speech_Actor_11.zip
+rm Video_Speech_Actor_12.zip
+rm Video_Speech_Actor_13.zip
+rm Video_Speech_Actor_14.zip
+rm Video_Speech_Actor_15.zip
+rm Video_Speech_Actor_16.zip
+rm Video_Speech_Actor_17.zip
+rm Video_Speech_Actor_18.zip
+rm Video_Speech_Actor_19.zip
+rm Video_Speech_Actor_20.zip
+rm Video_Speech_Actor_21.zip
+rm Video_Speech_Actor_22.zip
+rm Video_Speech_Actor_23.zip
+rm Video_Speech_Actor_24.zip
